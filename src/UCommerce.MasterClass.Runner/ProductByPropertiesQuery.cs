f499112bc4b71d97b7916a77f12706daad6961a7
Vasiliy Fomichev <vasiliy.foomichev@verndale.com>
2016-01-21 07:27:40 -0800

    Sample order queries.

---
+++ src/UCommerce.MasterClass.Runner/ProductByPropertiesQuery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Linq;
+using UCommerce.EntitiesV2;
+using UCommerce.EntitiesV2.Queries;
+using Product = UCommerce.Documents.Product;
+
+namespace UCommerce.MasterClass.Integration
+{
+    public class ProductByPropertiesQuery : ICannedQuery<UCommerce.EntitiesV2.Product>
+    {
+        private readonly string _fieldName;
+        private readonly string _propertyValue;
+        
+        public ProductByPropertiesQuery(string fieldName, string value)
+        {
+            _fieldName = fieldName;
+            _propertyValue = value;
+        }
+
+        public IEnumerable<EntitiesV2.Product> Execute(NHibernate.ISession session)
+        {
+            return session.Query<UCommerce.EntitiesV2.Product>()
+                .Where(product => product.ProductProperties.Any(
+                    property=>property.Value == _propertyValue &&
+                    property.ProductDefinitionField.Name == _fieldName));
+        }
+    }
+}
--- src/UCommerce.MasterClass.Runner/Program.cs
+++ src/UCommerce.MasterClass.Runner/Program.cs
@@ -21,12 +21,63 @@ namespace UCommerce.MasterClass.Integration
             //    .Resolve<IRepository<PurchaseOrder>>()
             //    .Select(new LatestOrderQuery()).ToList();
 
-		    var orders = UCommerce.EntitiesV2.PurchaseOrder.All().ToList();
-		    var repository = ObjectFactory.Instance.Resolve<IRepository<PurchaseOrder>>();
-		    var orders1 = repository.Select().ToList(); // same data as above
-		    var sessionProvider = ObjectFactory.Instance.Resolve<ISessionProvider>();
-		    var session = sessionProvider.GetSession();
-		    var order2 = session.Query<PurchaseOrder>().Where(o=>o.Customer!=null).ToList(); // another way to get orders
+            //var orders = UCommerce.EntitiesV2.PurchaseOrder.All().ToList();
+            //var repository = ObjectFactory.Instance.Resolve<IRepository<PurchaseOrder>>();
+            //var orders1 = repository.Select().ToList(); // same data as above
+            //var sessionProvider = ObjectFactory.Instance.Resolve<ISessionProvider>();
+            //var session = sessionProvider.GetSession();
+            //var order2 = session.Query<PurchaseOrder>().Where(o=>o.Customer!=null).ToList(); // another way to get orders
+
+            ////editing products
+            //var product1 = session.Query<Product>().FirstOrDefault();
+            //product1.Name = "hello world";
+            //product1.Save();
+
+            //var products = ObjectFactory.Instance.Resolve<IRepository<Product>>()
+            //    .Select(new ProductByPropertiesQuery("ShowOnHomepage", "True")).ToList();
+
+            //var orderLineRepository = ObjectFactory.Instance.Resolve<IRepository<OrderLine>>();
+            //var productReposityrory = ObjectFactory.Instance.Resolve<IRepository<Product>>();
+
+
+            //var query = from orderLine in orderLineRepository.Select()
+            //    join product in productReposityrory.Select()
+            //        on new
+            //        {
+            //            orderLine.Sku,
+            //            orderLine.VariantSku
+
+            //        } equals new
+            //        {
+            //            product.Sku,
+            //            product.VariantSku
+            //        }
+            //    select product;
+            //var result = query.ToList();
+
+            ////deferred executed
+            //var repo = ObjectFactory.Instance.Resolve<IRepository<PurchaseOrder>>();
+            //var orders2 = repo.Select();
+
+            //// requesting extra info 
+            ////fetch customer to avoid loading multiple queries for each
+            ////of the orders in the list.
+            //orders2.Fetch(x => x.Customer);
+            //orders2.FetchMany(x => x.OrderLines);
+
+            //var result1 = orders.ToList();
+
+            ////also deferred executed
+            //var secondQuery = result1.AsQueryable();
+
+            //foreach (var purchaseOrder in result1)
+            //{
+            //    if (purchaseOrder.Customer != null)
+            //    {
+            //        var customer = purchaseOrder.Customer;
+            //        var firstName = customer.FirstName;
+            //    }
+            //}
 
 
 
